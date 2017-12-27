2dbd8cc00bd02433a58244e0975b92d39d2c8092
Natti Katz <katzy687@gmail.com>
2017-12-27 21:44:54 +0200

    modified readme

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Gili's Towing Service Repo
 
 Probably the best towservice in Israel. You heard it here first.
+
+To run:
+Git Clone, npm install, gatsby develop
--- src/pages/index.he.js
+++ src/pages/index.he.js
@@ -6,7 +6,7 @@ const IndexPage = () => (
   <div>
     <h1>מה קורא???</h1>
     <p>ברוך הבא לאתר שלי. קורא'ם לי גילי</p>
-    <p>אני אוהב מכוניות וזונות</p>
+    <p>אני חולה על מכוניות וזונות</p>
     <div className="truck-wrapper" style={{ textAlign: "center" }}>
       <img src={truckImg} alt="truck" />
     </div>
