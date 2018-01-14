a7d917da6377936ee25d814bff11cdae5fdaffe1
Natti Katz <katzy687@gmail.com>
2018-01-14 14:39:01 +0200

    footer conditional added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,15 +2,33 @@ import React from 'react';
 import Link from 'gatsby-link';
 import PhoneButton from './PhoneButton';
 
-const Footer = () => (
-  <div style={styles} >
-    <p>I am very involved with the local Harley Davidson Association and we provide lots of services in the community.</p>
-    <p>Check out my <Link to="/community/" style={footerLinkStyles}> community service page </Link> for more details.</p>
-    <p>You can reach me by email at <a style={footerLinkStyles} href="">GiliLovesBlondes@gmail.com</a></p>
-    <p>Or you can always call</p>
-    <PhoneButton />
-  </div>
-);
+const Footer = (props) => {
+
+  const englishContent = (
+    <div style={styles} >
+      <p>I am very involved with the local Harley Davidson Association and we provide lots of services in the community.</p>
+      <p>Check out my <Link to="/community/" style={footerLinkStyles}> community service page </Link> for more details.</p>
+      <p>You can reach me by email at <a style={footerLinkStyles} href="">GiliLovesBlondes@gmail.com</a></p>
+      <p>Or you can always call</p>
+      <PhoneButton />
+    </div>
+  );
+
+  console.log(englishContent);
+
+  const hebrewContent = (
+    <div style={styles} >
+      <p>Hebrew text goes here</p>
+      <p>Check out my <Link to="/community/" style={footerLinkStyles}> community service page </Link> for more details.</p>
+      <p>You can reach me by email at <a style={footerLinkStyles} href="">GiliLovesBlondes@gmail.com</a></p>
+      <p>Or you can always call</p>
+      <PhoneButton />
+    </div>
+  );
+
+  return props.langKey === 'en' ? englishContent : hebrewContent;
+
+}
 
 export default Footer;
 
